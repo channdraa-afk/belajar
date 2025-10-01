9239a81d45216ca8910b2c5856585f935d4e702f
channdraa-afk <iamchan761@gmail.com>
2025-10-01 14:04:14 +0700

    Update app_steamlit.py

--- app_steamlit.py
+++ app_steamlit.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 st.title("Belajar Streamlit")
 st.markdown("Perkenalkan nama saya Channdraaa")
+st.header("Saya masih binggung dikit cara pakai streamlit ini :/")
 
 st.balloons()
 st.snow()
+st.success("Selamat Project pertamanya")
 
 
 
